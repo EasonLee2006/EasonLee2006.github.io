fed732f31a7fb3460d3a315a2a7902359e37f408
EasonLee2006 <108567901+EasonLee2006@users.noreply.github.com>
2024-02-13 21:56:31 +0800

    buttons position ready

--- whatIf/script.js
+++ whatIf/script.js
@@ -1,21 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sentenceElement = document.getElementById('sentence');
-    const changeButton = document.getElementById('changeButton');
+    const button1 = document.getElementById('button1');
     
     // Array of sentences
     const sentences = [
-      'I like apples.',
-      'I love bananas.',
-      'Hello world!'
+        'I like apples.',
+        'I love bananas.',
+        'Hello world!'
     ];
     let currentSentenceIndex = 1;
-  
-    changeButton.addEventListener('click', function () {
-      // Change the sentence
-      sentenceElement.textContent = sentences[currentSentenceIndex];
-  
-      // Increment the index or loop back to 0 if it exceeds the length of the array
-      currentSentenceIndex = (currentSentenceIndex + 1) % sentences.length;
+    
+    button1.addEventListener('click', function () {
+        let button1CurrentFontSize = parseFloat(window.getComputedStyle(button1).fontSize);
+        let button1CurrentPadding = parseFloat(window.getComputedStyle(button1).padding);
+        
+        button1CurrentFontSize += 8;
+        button1CurrentPadding += 8;
+
+        button1.style.fontSize = button1CurrentFontSize + 'px';
+        button1.style.padding = button1CurrentPadding + 'px';
+        
+        // Change the sentence
+        sentenceElement.textContent = sentences[currentSentenceIndex];
+        
+        button1.textContent = "bigger";
+        
+        // Increment the index or loop back to 0 if it exceeds the length of the array
+        currentSentenceIndex = (currentSentenceIndex + 1) % sentences.length;
     });
   });
   
