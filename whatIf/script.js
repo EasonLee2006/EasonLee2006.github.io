269122849bfdbbf8626f65223d40eca9ce379524
EasonLee2006 <108567901+EasonLee2006@users.noreply.github.com>
2024-02-14 19:28:07 +0800

    a working version completed!!

--- whatIf/script.js
+++ whatIf/script.js
@@ -7,24 +7,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const defaultFontSize = 16;
     const defaultPaddingSize = 10;
     let gifUrls = [];
-    for(let i=0 ; i<=9 ; i++){
+    for(let i=0 ; i<=10 ; i++){
         gifUrls[i] = 'pics/' + i + '.gif'
     }
 
     // Array of sentences
     const sentences = [
-        'Will you be my valentine?',//0
+        'Will you be my Valentine?',//0
         'Maybe think twice?',//1
         'Please?',//2
         'I might cry',//3
         'Think one more time?',//4
-        'I beg you',//5
+        'I\'m begging you',//5
         'One last time, please?',//6
-        'Ok, here\'s something',//7
-        'get rickrolled',//8
-        'Thank you, love you'//9
+        'Ok, but before goodbye, here\'s something',//7
+        'Get rickrolled lol',//8
+        'Thank you. I love you',//9
+        'Here\'s the sorce code, thanks for your participating. Refresh page to play again' //10
     ];
-    let SentenceIndex = 0;
+
     let phase = 0;
     
     function preloadGifs(urls) {
@@ -60,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 break;
             
             case 2:
-                b1FontSize = defaultFontSize+8;
+                b1FontSize = defaultFontSize+10;
                 b1Padding = defaultPaddingSize+8;
                 b1TextContent = "yes";
 
@@ -69,8 +70,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 b2TextContent = "no";
                 break;
             case 3:
-                b1FontSize = defaultFontSize + 10;
-                b1Padding = defaultPaddingSize + 10;
+                b1FontSize = defaultFontSize + 16;
+                b1Padding = defaultPaddingSize + 12;
                 b1TextContent = "ok fine";
 
                 b2FontSize = defaultFontSize;
@@ -78,26 +79,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 b2TextContent = "nope";
                 break;
             case 4:
-                b1FontSize = defaultFontSize+14;
-                b1Padding = defaultPaddingSize+14;
-                b1TextContent = "yes";
+                b1FontSize = defaultFontSize+22;
+                b1Padding = defaultPaddingSize+16;
+                b1TextContent = "sure";
 
                 b2FontSize = defaultFontSize;
                 b2Padding = defaultPaddingSize;
                 b2TextContent = "nope";
                 break;
             case 5:
-                b1FontSize = defaultFontSize+16;
-                b1Padding = defaultPaddingSize+16;
-                b1TextContent = "yes";
+                b1FontSize = defaultFontSize+28;
+                b1Padding = defaultPaddingSize+20;
+                b1TextContent = "ok";
 
                 b2FontSize = defaultFontSize;
                 b2Padding = defaultPaddingSize;
                 b2TextContent = "nope";
                 break;
             case 6:
-                b1FontSize = defaultFontSize+18;
-                b1Padding = defaultPaddingSize+18;
+                b1FontSize = defaultFontSize+34;
+                b1Padding = defaultPaddingSize+24;
                 b1TextContent = "yes";
 
                 b2FontSize = defaultFontSize;
@@ -125,11 +126,20 @@ document.addEventListener('DOMContentLoaded', function () {
             case 9:
                 b1FontSize = defaultFontSize;
                 b1Padding = defaultPaddingSize;
-                b1TextContent = "love you";
+                b1TextContent = "love you too";
 
                 b2FontSize = defaultFontSize;
                 b2Padding = defaultPaddingSize;
-                b2TextContent = "love you";
+                b2TextContent = "i'm just kidding";
+                break;
+            case 10:
+                b1FontSize = defaultFontSize;
+                b1Padding = defaultPaddingSize;
+                b1TextContent = "sorce";
+
+                b2FontSize = defaultFontSize;
+                b2Padding = defaultPaddingSize;
+                b2TextContent = "code";
                 break;
             default:
                 break;
@@ -148,12 +158,16 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     b1.addEventListener('click', function () {
-        if(phase == 7 || phase == 8){
-            phase = 8;
-        }else if(phase != 0 && phase != 9){
-            phase = 0;
-        }else{
+        if(phase == 0){
             phase = 9;
+        }else if( 1<=phase && phase<=6 ){
+            phase = 0;
+        }else if( phase == 7){
+            phase = 8;
+        }else if( phase == 10 ){
+            window.open('https://github.com/EasonLee2006/EasonLee2006.github.io/tree/main/whatIf', '_blank');
+        }else{
+            phase = 10;
         }
         the_result();
     });
@@ -161,6 +175,12 @@ document.addEventListener('DOMContentLoaded', function () {
     b2.addEventListener('click', function () {
         if(phase <=7){
             phase+=1;
+        }else if(phase == 9){
+            phase = 7;
+        }else if (phase == 10){
+            window.open('https://github.com/EasonLee2006/EasonLee2006.github.io/tree/main/whatIf', '_blank');
+        }else{
+            phase = 10;
         }
         the_result();
     });
